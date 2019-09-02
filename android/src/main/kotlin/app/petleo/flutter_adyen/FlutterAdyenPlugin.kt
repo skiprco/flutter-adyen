a68535ef5009b7ad462fa08c2f0fd746a1a14f14
Junus Ergin <junus91@googlemail.com>
2019-09-02 14:49:08 +0200

    Initial commit

---
+++ android/src/main/kotlin/app/petleo/flutter_adyen/FlutterAdyenPlugin.kt
@@ -0,0 +1,25 @@
+package app.petleo.flutter_adyen
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FlutterAdyenPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "flutter_adyen")
+      channel.setMethodCallHandler(FlutterAdyenPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
---
+++ ios/flutter_adyen.podspec
@@ -0,0 +1,21 @@
+#
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+Pod::Spec.new do |s|
+  s.name             = 'flutter_adyen'
+  s.version          = '0.0.1'
+  s.summary          = 'Flutter plugin to integrate with the Android and iOS libraries of Adyen.'
+  s.description      = <<-DESC
+Flutter plugin to integrate with the Android and iOS libraries of Adyen.
+                       DESC
+  s.homepage         = 'http://example.com'
+  s.license          = { :file => '../LICENSE' }
+  s.author           = { 'Your Company' => 'email@example.com' }
+  s.source           = { :path => '.' }
+  s.source_files = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
+  s.dependency 'Flutter'
+
+  s.ios.deployment_target = '8.0'
+end
+
