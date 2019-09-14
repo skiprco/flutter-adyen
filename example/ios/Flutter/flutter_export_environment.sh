05ec27e5a36fd3a637cd9ce5c200207eff80605b
bulenthacioglu <bulent.hacioglu@hotmail.com>
2019-09-14 14:48:48 +0200

    handling amount from dart

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -6,4 +6,3 @@ export "FLUTTER_TARGET=/Users/bulenthacioglu/IdeaProjects/flutter-adyen/example/
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/bulenthacioglu/sdk/flutter/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
