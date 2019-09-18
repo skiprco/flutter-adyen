9317f6fe509c61e6fa241f9e498e925ee8bc0a56
bulenthacioglu <bulent.hacioglu@hotmail.com>
2019-09-18 17:32:43 +0200

    iOS ADYEN DONE!

--- ios/Classes/SwiftFlutterAdyenPlugin.swift
+++ ios/Classes/SwiftFlutterAdyenPlugin.swift
@@ -22,10 +22,11 @@ public class SwiftFlutterAdyenPlugin: NSObject, FlutterPlugin {
     var shopperReference: String?
     var reference: String?
     var mResult: FlutterResult?
-
+    var topController: UIViewController?
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         guard call.method.elementsEqual("openDropIn") else { return }
-
+        
         let arguments = call.arguments as? [String: Any]
         let paymentMethodsResponse = arguments?["paymentMethods"] as? String
         baseURL = arguments?["baseUrl"] as? String
@@ -37,20 +38,25 @@ public class SwiftFlutterAdyenPlugin: NSObject, FlutterPlugin {
         shopperReference = arguments?["shopperReference"] as? String
         reference = arguments?["reference"] as? String
         mResult = result
-
+        
         guard let paymentData = paymentMethodsResponse?.data(using: .utf8),
             let paymentMethods = try? JSONDecoder().decode(PaymentMethods.self, from: paymentData) else {
-            return
+                return
         }
-
+        
         let configuration = DropInComponent.PaymentMethodsConfiguration()
         configuration.card.publicKey = pubKey
         dropInComponent = DropInComponent(paymentMethods: paymentMethods, paymentMethodsConfiguration: configuration)
         dropInComponent?.delegate = self
         dropInComponent?.environment = .test
-
+        
+        //        topController = UIApplication.shared.keyWindow?.rootViewController
+        //        while let presentedViewController = topController?.presentedViewController {
+        //            topController = presentedViewController
+        //        }
         if var topController = UIApplication.shared.keyWindow?.rootViewController {
-            while let presentedViewController = topController.presentedViewController {
+            self.topController = topController
+            while let presentedViewController = topController.presentedViewController{
                 topController = presentedViewController
             }
             topController.present(dropInComponent!.viewController, animated: true)
@@ -74,12 +80,12 @@ extension SwiftFlutterAdyenPlugin: DropInComponentDelegate {
                                    "channel": "iOS",
                                    "merchantAccount": merchantAccount,
                                    "reference": reference,
-                                   "returnUrl": "iATROS://",
+                                   "returnUrl": "iatrosPayment://",
                                    "storePaymentMethod": false,
                                    "additionalData": ["allow3DS2":"false"]]
         
         let jsonData = try? JSONSerialization.data(withJSONObject: json)
-
+        
         request.httpBody = jsonData
         URLSession.shared.dataTask(with: request) { data, response, error in
             print("RESPONSE IS  ", response)
@@ -87,42 +93,62 @@ extension SwiftFlutterAdyenPlugin: DropInComponentDelegate {
             if(data != nil) {
                 self.finish(data: data!, component: component)
             }
-        }.resume()
+            }.resume()
     }
-
+    
     func finish(data: Data, component: DropInComponent) {
-        print("ACT IS FINSH CALLED")
-        let decodedString = String(data: data, encoding: .utf8)!
         let paymentResponseJson = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String,Any>
-        if((paymentResponseJson) != nil) {
+        print("ACT IS FINSH CALLED  ", paymentResponseJson)
+        if ((paymentResponseJson) != nil) {
             let action = paymentResponseJson?!["action"]
-                  let act = try? JSONDecoder().decode(Action.self, from: JSONSerialization.data(withJSONObject: action, options: .fragmentsAllowed)) as! Action
-            print("ACT IS ", act)
-                  if(act != nil){
-                      component.handle(act!)
-                  } else {
-                    print("ACT IS 1 ")
-                      let resultCode = try? paymentResponseJson!!["resultCode"] as! String
-                    print("ACT IS RESULTCODE ", resultCode)
-                      let success = resultCode == "Authorised" || resultCode == "Received" || resultCode == "Pending"
-                        component.stopLoading()
-                        if (success) {
-                            self.mResult!("SUCCESS")
-                        } else {
-                            self.mResult!("Failed with result code \(resultCode)")
+            print("ACTION IS ", action)
+            if(action != nil) {
+                let act = try? JSONDecoder().decode(Action.self, from: JSONSerialization.data(withJSONObject: action, options: .sortedKeys)) as! Action
+                print("ACT IS ", act)
+                if(act != nil){
+                    component.handle(act!)
+                }
+            } else {
+                print("PAYMENT RESPONSE JSON IS ", paymentResponseJson)
+                let resultCode = try? paymentResponseJson!!["resultCode"] as! String
+                print("RESULT CODE IS ", resultCode)
+                let success = resultCode == "Authorised" || resultCode == "Received" || resultCode == "Pending"
+                print("success IS ", success)
+                component.stopLoading()
+                if (success) {
+                    print("succesSFULL!!!!")
+                    self.mResult!("SUCCESS")
+                    DispatchQueue.global(qos: .background).async {
+                        
+                        // Background Thread
+                        DispatchQueue.main.async {
+                            self.topController?.dismiss(animated: false, completion: nil)
                         }
-                  }
+                    }
+                } else {
+                    print("FAIL!!!!")
+                    self.mResult!("Failed with result code \(resultCode)")
+                    DispatchQueue.global(qos: .background).async {
+                        
+                        // Background Thread
+                        
+                        DispatchQueue.main.async {
+                            self.topController?.dismiss(animated: false, completion: nil)
+                        }
+                    }
+                }
+            }
         }
     }
     
     public func didProvide(_ data: ActionComponentData, from component: DropInComponent) {
         print("FUCKING BULENT")
-         guard let baseURL = baseURL, let url = URL(string: baseURL + "payments/details/") else { return }
-               var request = URLRequest(url: url)
-               request.httpMethod = "POST"
-               request.setValue("\(authToken!)", forHTTPHeaderField: "Authorization")
-               request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-               
+        guard let baseURL = baseURL, let url = URL(string: baseURL + "payments/details/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("\(authToken!)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
         let json: [String: Any] = ["details": data.details.dictionaryRepresentation,"paymentData": data.paymentData]
         let jsonData = try? JSONSerialization.data(withJSONObject: json)
         request.httpBody = jsonData
@@ -130,9 +156,9 @@ extension SwiftFlutterAdyenPlugin: DropInComponentDelegate {
             if(data != nil) {
                 self.finish(data: data!, component: component)
             }
-        }.resume()
+            }.resume()
     }
-
+    
     public func didFail(with error: Error, from component: DropInComponent) {
         print("FUCKING JUNUS")
         
@@ -141,29 +167,29 @@ extension SwiftFlutterAdyenPlugin: DropInComponentDelegate {
 }
 
 extension UIViewController: PaymentComponentDelegate {
-
+    
     public func didSubmit(_ data: PaymentComponentData, from component: PaymentComponent) {
         print("FUCKING JULIAN")
         //performPayment(with: public  }
     }
-
+    
     public func didFail(with error: Error, from component: PaymentComponent) {
         print("FUCKING CERI")
         //performPayment(with: public  }
     }
-
+    
 }
 
 extension UIViewController: ActionComponentDelegate {
-
+    
     public func didFail(with error: Error, from component: ActionComponent) {
         print("FUCKING PATRICK")
         //performPayment(with: public  }
     }
-
+    
     public func didProvide(_ data: ActionComponentData, from component: ActionComponent) {
         print("FUCKING SHAWKY")
         //performPayment(with: public  }
     }
-
+    
 }
